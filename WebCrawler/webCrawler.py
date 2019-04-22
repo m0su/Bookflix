6ee6d03dc4f2f1f31508b72a15c02d36378fba2c
SEO <sirm0su@gmail.com>
2019-04-22 16:29:20 +0900

    웹 크롤러 구성중

--- WebCrawler/webCrawler.py
+++ WebCrawler/webCrawler.py
@@ -1,6 +1,7 @@
-import requests
 from bs4 import BeautifulSoup
+import requests
 
+lis = []
 
 def get_html(url):
   _html = ""
@@ -9,32 +10,24 @@ def get_html(url):
     _html = resp.text
     return _html
 
-
 def parse_html(html):
-  book_list = list()
-  soup = BeautifulSoup(html, 'html.parser', from_encoding='utf-8')
-  # book_area = soup.find_all("div", {"class": "tit3"})
-  book_area = soup.select("div", {"class": "tit3"})
-  # translate_data(book_area)
-  return book_area
-  for book_index in book_area:
-    info_soup = book_index.find("a")
-    _url = info_soup["href"]
-    _text = info_soup.title
-    _title = ""
-    _num = _text[0]
-    if len(_text) > 1:
-      _title = _text[1]
+  bs = BeautifulSoup(html, 'html.parser')
+  book_area = bs.find("table", {"class":"list_ranking"})
+  books = book_area.findAll("div", {"class":"tit3"})
+  rank = 0
+  for book in books:
+    rank += 1
+    title = book.find('a')['title']
+    img = "https://movie.naver.com/" +book.find('a')['href']
+    lis.append([rank, title, img])
+  return lis
 
-      book_list.append((_num, _title, _url, ))
-  return book_list
 
-def translate_data(input):
-  trans = input.encode('utf-8')
-  trans = trans.decode('unicode_escape')
-  return trans
 
-url = 'https://movie.naver.com/movie/sdb/rank/rmovie.nhn'
+# url = 'https://movie.naver.com/movie/sdb/rank/rmovie.nhn'
+url = "https://movie.naver.com/movie/sdb/rank/rmovie.nhn"
+# url = 'https://pyony.com/ebooks/ridiselect/'
 html = get_html(url)
 data = parse_html(html)
 print(data)
+
